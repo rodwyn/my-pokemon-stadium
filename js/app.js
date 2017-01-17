97364f9469e760b23b337e858622bd7750db2f0b
Rodwyn Moreno <rodwyn.moreno@the-cocktail.com>
2017-01-17 11:27:32 -0600

    Implemented attack method.

--- js/app.js
+++ js/app.js
@@ -10,6 +10,32 @@ new Vue({
       this.gameIsRunnig = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
-    }
+    },
+    attack: function() {
+      var max = 9;
+      var min = 3;
+      var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monsterHealth -= damage;
+
+      if (this.monsterHealth <=0) {
+        alert('You Won!');
+        this.gameIsRunnig = false;
+        return;
+      }
+
+      max = 10;
+      min = 2;
+      damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.playerHealth -= damage;
+
+      if (this.playerHealth <=0) {
+        alert('You Lost!');
+        this.gameIsRunnig = false;
+        return;
+      }
+    },
+    specialAttack: function() {},
+    heal: function() {},
+    giveUp: function() {}
   }
 });
