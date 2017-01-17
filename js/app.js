db59b006a8984196876b02decac825d761de6dbc
Rodwyn Moreno <rodwyn.moreno@the-cocktail.com>
2017-01-17 11:08:03 -0600

    Initialized vue.

---
+++ js/app.js
@@ -0,0 +1,15 @@
+new Vue({
+  el: '#app',
+  data: {
+    playerHealth: 100,
+    monsterHealth: 100,
+    gameIsRunnig: false
+  },
+  methods: {
+    // startGame: function() {
+    //   this.gameIsRunnig = true;
+    //   this.playerHealth = 100;
+    //   this.monsterHealth = 100;
+    // }
+  }
+});
